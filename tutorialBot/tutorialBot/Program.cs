ab7481ea8fe0ef36caa3611368a8fca2314d8313
Ben <crawfordben26@gmail.com>
2018-03-01 22:40:20 +0000

    First Commit

---
+++ tutorialBot/tutorialBot/Program.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace tutorialBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private DiscordSocketClient _client;
+        private CommandService _command;
+        private IServiceProvider _service;
+
+        public async Task RunBotAsync()
+        {
+
+        }
+    }
+}
