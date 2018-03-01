79b5fb0124ba47f12a4759bcc9ffef937b97e59b
Ben <crawfordben26@gmail.com>
2018-03-01 23:15:50 +0000

    Basic working bot

--- tutorialBot/tutorialBot/Program.cs
+++ tutorialBot/tutorialBot/Program.cs
@@ -1,23 +1,75 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace tutorialBot
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
-        private CommandService _command;
-        private IServiceProvider _service;
+        private CommandService _commands;
+        private IServiceProvider _services;
 
         public async Task RunBotAsync()
         {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
 
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botToken =  "NDE4ODg2NzI5OTUzNzA1OTk1.DXoVQA.8c_ImD9VyXfgpw4W-FdRzdtexbQ";
+
+            //Event subscriptions
+            _client.Log += log;
+
+            await RegisterCommandAsync();
+
+            await _client.LoginAsync(Discord.TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot) return;
+
+            int argpos = 0;
+
+            if (message.HasStringPrefix("!tst", ref argpos) || message.HasMentionPrefix(_client.CurrentUser, ref argpos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argpos, _services);
+
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            } 
         }
     }
 }
